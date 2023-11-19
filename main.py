4396d3ef2934a69248da193e35223f031ca97193
rk00717 <58892434+rk00717@users.noreply.github.com>
2023-11-19 14:55:01 +0530

    Completed third project

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from rkode.madlib_module import Madlib
 from rkode.guessthenumber_module import GuessTheNumber
+from rkode.rockpaperscissor_module import RockPaperScissor
 
 def play_madlib():
-    madlib_instance = Madlib()
-    madlib_instance.get_new_madlib()
+    game_instance = Madlib()
+    game_instance.get_new_madlib()
 
 def play_guessthenumber():
-    guessthenumber_instance = GuessTheNumber()
-    guessthenumber_instance.start_game()
+    game_instance = GuessTheNumber()
+    game_instance.start_game()
+
+def play_rockpaperscissor():
+    game_instance = RockPaperScissor()
+    game_instance.start_game()
 
 if __name__ == "__main__":
     # play_madlib()
-    play_guessthenumber()
+    # play_guessthenumber()
+    play_rockpaperscissor()
---
+++ rkode/rockpaperscissor_module.py
@@ -0,0 +1,36 @@
+from random import choice
+
+class RockPaperScissor:
+    win_condition = [
+        ['r', 's'],
+        ['s', 'p'],
+        ['p', 'r']
+    ]
+
+    abbs = {
+        "r" : "rock",
+        "p" : "paper",
+        "s" : "scissor"
+    }
+
+    def is_win(self, p1, p2):
+        if [p1, p2] in self.win_condition:
+            return True
+        else:
+            return False
+
+    def start_game(self):
+        system_choice = choice(['r', 'p', 's'])
+        user_choice = input('Choose Rock(R), Paper(P), or Scissor(S) : ').lower()
+
+        display_result = "null"
+        if(user_choice == system_choice):
+            display_result = "Match Draw"
+        else:
+            display_result = f"You {('Won' if(self.is_win(user_choice, system_choice)) else 'Lost')} by Me"
+
+        print(f"{display_result}\n My choice : {self.abbs[system_choice]} \n Your choice : {self.abbs[user_choice]}")
+
+        preference = input("Do you want to play again? (Y/N) : ").lower()
+        if(preference == 'y'):
+            self.start_game()
